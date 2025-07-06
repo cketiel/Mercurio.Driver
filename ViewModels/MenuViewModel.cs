8840165b09680fcc535487c5854cc6cb52080336
cketiel <cketielbr@gmail.com>
2025-07-06 06:55:46 -0600

    Add MenuViewModel

---
+++ ViewModels/MenuViewModel.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Mercurio.Driver.Views;
+
+namespace Mercurio.Driver.ViewModels
+{
+    public partial class MenuViewModel : ObservableObject
+    {
+        [RelayCommand]
+        private async Task GoToSchedule()
+        {
+            // Close the flyout menu and navigate to the SchedulePage
+            await Shell.Current.GoToAsync($"//{nameof(SchedulePage)}");
+        }
+
+        [RelayCommand]
+        private async Task SignOut()
+        {
+            // Logout logic
+            Preferences.Clear(); 
+            await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
+        }
+    }
+}
