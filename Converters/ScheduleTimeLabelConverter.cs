59257f7690379fb14c9065b90139bf65e5dc0bd4
cketiel <cketielbr@gmail.com>
2025-07-06 02:16:28 -0600

    Add ScheduleTimeLabelConverter

---
+++ Converters/ScheduleTimeLabelConverter.cs
@@ -0,0 +1,21 @@
+﻿
+
+using System.Globalization;
+using Mercurio.Driver.DTOs;
+using Mercurio.Driver.Models;
+
+namespace Mercurio.Driver.Converters
+{
+    // Convert time label (SCH TIME or APT TIME)
+    public class ScheduleTimeLabelConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not ScheduleDto schedule) return string.Empty;
+
+            return schedule.EventType == ScheduleEventType.Pickup ? "SCH TIME" : "APT TIME";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
