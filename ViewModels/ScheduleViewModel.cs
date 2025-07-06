23c37d1640e4cbebe433e576d825ed88ae2a977e
cketiel <cketielbr@gmail.com>
2025-07-05 21:55:06 -0600

    Add Schedule ViewModel

---
+++ ViewModels/ScheduleViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mercurio.Driver.ViewModels
+{
+    internal class ScheduleViewModel
+    {
+    }
+}
