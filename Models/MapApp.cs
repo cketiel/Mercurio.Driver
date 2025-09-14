ad0b367671fb81cc102e7571d329f9837beb99b2
cketiel <cketielbr@gmail.com>
2025-09-14 04:55:56 -0600

    Add MapApp class model

---
+++ Models/MapApp.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mercurio.Driver.Models
+{
+    public partial class MapApp : ObservableObject
+    {
+        public string Name { get; set; }
+        public string Icon { get; set; } // Image file name in Resources/Images
+        public string Scheme { get; set; } // Unique identifier (URL Scheme for iOS, Package Name for Android)
+
+        [ObservableProperty]
+        private bool isSelected;
+    }
+}
