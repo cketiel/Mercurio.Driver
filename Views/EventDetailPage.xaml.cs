436e0788106dc0b09d8d2fc0e37f78196785ada7
cketiel <cketielbr@gmail.com>
2025-09-16 00:29:46 -0600

    Udate EventDetailPage

--- Views/EventDetailPage.xaml.cs
+++ Views/EventDetailPage.xaml.cs
@@ -1,9 +1,13 @@
+
+using Mercurio.Driver.ViewModels;
+
 namespace Mercurio.Driver.Views;
 
 public partial class EventDetailPage : ContentPage
 {
-	public EventDetailPage()
-	{
-		InitializeComponent();
-	}
+    public EventDetailPage(EventDetailPageViewModel viewModel)
+    {
+        InitializeComponent();
+        BindingContext = viewModel;
+    }
 }
