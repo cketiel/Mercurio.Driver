2d384e37406d59f6099d94a4cd72120adb1283c9
cketiel <cketielbr@gmail.com>
2025-09-16 03:48:11 -0600

    Add comments in SignatureViewModel for better understanding

--- ViewModels/SignatureViewModel.cs
+++ ViewModels/SignatureViewModel.cs
@@ -26,12 +26,16 @@ namespace Mercurio.Driver.ViewModels
         {
             IsBusy = true;
             try
-            {               
+            {
+                // Convert the signature to a stream image
                 var signatureStream = await DrawingView.GetImageStream(Lines, new Size(600, 300), Colors.White);
 
+                // Convert stream to byte[]
                 using var memoryStream = new MemoryStream();
                 await signatureStream.CopyToAsync(memoryStream);
                 var signatureBytes = memoryStream.ToArray();
+
+                // Convert the bytes to Base64 to send it through the API
                 var signatureBase64 = Convert.ToBase64String(signatureBytes);
 
                 // TODO: Llamar al servicio real para enviar la firma al backend.
@@ -43,6 +47,7 @@ namespace Mercurio.Driver.ViewModels
 
                 if (success)
                 {
+                    // We return to the previous page, passing a parameter to indicate that it was saved
                     await Shell.Current.GoToAsync("..", new Dictionary<string, object>
                     {
                         { "SignatureSaved", true }
@@ -77,6 +82,11 @@ namespace Mercurio.Driver.ViewModels
             var activity = Platform.CurrentActivity;
             if (activity != null)
                 activity.RequestedOrientation = Android.Content.PM.ScreenOrientation.Landscape;
+
+#elif IOS    
+            // On iOS, this must be configured at the app level or with more complex code.
+            // The simplest way is to allow all orientations in Info.plist
+            // and let the UI adapt.
 #endif
         }
 
