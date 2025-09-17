7628aea1c37f7691d377dc83659c7f36c7201f0a
cketiel <cketielbr@gmail.com>
2025-09-17 03:44:19 -0600

    Show loading on Schedule while loading TodaySchedule data

--- ViewModels/ScheduleViewModel.cs
+++ ViewModels/ScheduleViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using IntelliJ.Lang.Annotations;
 using Mercurio.Driver.Views; 
 using System.Diagnostics;
 using System.Web; 
@@ -14,23 +15,42 @@ namespace Mercurio.Driver.ViewModels
         [ObservableProperty]
         private string _vehicleLogin;
 
+        [ObservableProperty]
+        private bool _isBusy;
+
         public ScheduleViewModel() { }
 
         [RelayCommand]
         private async Task GoToTodaySchedule()
         {
+            if (IsBusy) return; // Avoid multiple clicks
+
             if (string.IsNullOrWhiteSpace(RunLogin))
             {
                 await Shell.Current.DisplayAlert("Input Required", "Please enter a Run Login.", "OK");
                 return;
             }
 
-            var navigationParameter = new Dictionary<string, object>
+            try
             {
-                { "runLogin", RunLogin }
-            };
-         
-            await Shell.Current.GoToAsync(nameof(TodaySchedulePage), navigationParameter);
+                IsBusy = true; 
+
+                var navigationParameter = new Dictionary<string, object>
+                {
+                    { "runLogin", RunLogin }
+                };
+
+                await Shell.Current.GoToAsync(nameof(TodaySchedulePage), navigationParameter);
+            }
+            catch (Exception ex)
+            {               
+                Debug.WriteLine($"Error while browsing: {ex.Message}");
+                await Shell.Current.DisplayAlert("Error", "The schedule page could not be opened.", "OK");
+            }
+            finally
+            {
+                IsBusy = false; 
+            }      
         }
 
         [RelayCommand]
