1c3ec8c28180d7aa2f1464402ed36c824d4069eb
cketiel <cketielbr@gmail.com>
2025-10-27 22:25:00 -0600

    Reduce live tracking to one second

--- Platforms/Android/Services/GpsServiceAndroid.cs
+++ Platforms/Android/Services/GpsServiceAndroid.cs
@@ -58,7 +58,7 @@ namespace Mercurio.Driver.Services
             _idVehicleRoute = idVehicleRoute;
             PromoteToForegroundService();
 
-            _timer = new System.Timers.Timer(5000); // 5 segundos
+            _timer = new System.Timers.Timer(1000); // 5 segundos
             _timer.Elapsed += async (s, e) => await SendLocationAsync();
             _timer.Start();
 
